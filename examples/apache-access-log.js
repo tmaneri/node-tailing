fba6ca4ae9ec244bb0926d6f5f47428fcb616d6e
tmaneri <tommaneri@gmail.com>
2016-11-16 15:03:03 -0800

    Update apache-access-log.js

--- examples/apache-access-log.js
+++ examples/apache-access-log.js
@@ -1,7 +1,7 @@
-var tailing = require('tailing');
+var Tailing = require('tailing');
 
 var pattern = /^(\S+) (\S+) (\S+) \[([^:]+):(\d+:\d+:\d+) ([^\]]+)\] \"(\S+) (.*?) (\S+)\" (\S+) (\S+) "([^"]*)" "([^"]*)"/i;
-var tail = tailing('/var/log/apache2/access.log');
+var tail = new Tailing('/var/log/apache2/access.log');
 
 tail.on('line', function(line) {
   var matches = line.match(pattern);
